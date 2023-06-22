9e39758cf6f8ed49992d8036b44ce79e78644c5c
Ethan Budreau <ethanbudreau@outlook.com>
2023-06-22 08:14:20 -0500

    switching to maven-build

---
+++ recipe_tracker/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.recipetracker.recipe_tracker {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.recipetracker.recipe_tracker to javafx.fxml;
+    exports com.recipetracker.recipe_tracker;
+}
