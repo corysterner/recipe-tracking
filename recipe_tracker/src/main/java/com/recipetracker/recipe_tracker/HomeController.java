c58771d035bd126ea19a28682bce397a472cb7ba
Ethan Budreau <ebudreau@epic.com>
2023-06-24 15:56:37 -0500

    Added recipe card

--- recipe_tracker/src/main/java/com/recipetracker/recipe_tracker/HomeController.java
+++ recipe_tracker/src/main/java/com/recipetracker/recipe_tracker/HomeController.java
@@ -1,9 +1,14 @@
 package com.recipetracker.recipe_tracker;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+
+import java.io.IOException;
 
 public class HomeController {
     public TextField searchTextAllRecipes;
@@ -16,14 +21,29 @@ public class HomeController {
     public ScrollPane getPages(int pageIndex){
         VBox vBox = new VBox();
         for(int i = pageIndex; i < pageIndex + 100; i++){
-            Label recipeTitle = new Label("Recipe " + i);
-            VBox recipeBox = new VBox(recipeTitle);
-            vBox.getChildren().add(recipeBox);
+
+            try{
+                FXMLLoader cardLoader = new FXMLLoader(getClass().getResource("recipe-card.fxml"));
+                AnchorPane cardRoot = cardLoader.load();
+                vBox.getChildren().add(cardRoot);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+            //Label recipeTitle = new Label("Recipe " + i);
+            //VBox recipeBox = new VBox(recipeTitle);
+
         }
         ScrollPane scrollPane = new ScrollPane(vBox);
         return scrollPane;
     }
     public void searchAllRecipes(ActionEvent actionEvent) {
         allRecipesPagination.setPageFactory(this::getPages);
+    }
+
+    public void getMyRecipes(Event event) {
+    }
+
+    public void getMyFavorites(Event event) {
     }
 }
---
+++ recipe_tracker/src/main/java/com/recipetracker/recipe_tracker/RecipeCardController.java
@@ -0,0 +1,8 @@
+package com.recipetracker.recipe_tracker;
+
+import javafx.scene.input.MouseEvent;
+
+public class RecipeCardController {
+    public void updateSingleRecipe(MouseEvent mouseEvent) {
+    }
+}
