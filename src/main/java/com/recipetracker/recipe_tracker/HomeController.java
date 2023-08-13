75eebc624651c90331159ca1a5166a0654617e29
Cory Sterner <corysterner@gmail.com>
2023-08-12 19:25:11 -0500

    Made fullRecipeCall into a single call for stored procedures. Added functionality for favorites, comments, and ratings.

--- src/main/java/com/recipetracker/recipe_tracker/HomeController.java
+++ src/main/java/com/recipetracker/recipe_tracker/HomeController.java
@@ -1,5 +1,6 @@
 package com.recipetracker.recipe_tracker;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -25,6 +26,7 @@ import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
     Integer userId = 0;
+    Recipe currentRecipe;
     public TextField searchTextAllRecipes;
     public ComboBox<Recipe.Category> categoryFilter;
     public ComboBox<Integer> ratingFilter;
@@ -48,6 +50,8 @@ public class HomeController implements Initializable {
     public Label categoryListLabel;
     public Rating ratingBar;
     public Label ratingLabel;
+    public ToggleButton toggleFavorite = new ToggleButton("Favorite");
+    public TextArea comments;
 
 
     List<Recipe.Category> allCatValues = new ArrayList<>();
@@ -79,6 +83,8 @@ public class HomeController implements Initializable {
         ratingList = getRatingList();
         availableRatings = FXCollections.observableList(ratingList);
         ratingFilter.setItems(availableRatings);
+
+        toggleFavorite.setStyle("-fx-base: gold;");
     }
 
     private class CategoryConverter extends StringConverter<Recipe.Category> {
@@ -165,11 +171,17 @@ public class HomeController implements Initializable {
         clearSingleRecipe();
 
         // Update the right side scroll pane with this recipe's info
-        Recipe recipe = DbConnector.dbConnector.selectQueryFullRecipe(recipeId);
+        Recipe recipe = DbConnector.dbConnector.selectQueryFullRecipe(recipeId, this.userId);
+        currentRecipe = recipe;
 
         singleRecipeTitle.setText(recipe.name);
         recipeDescription.setText(recipe.description);
-        ratingBar.setRating(recipe.getRatingAvg());
+        toggleFavorite.setSelected(recipe.isFavorite);
+        Platform.runLater(() ->{
+            ratingBar.setRating(recipe.getUserRating() == 0 ? recipe.getRatingAvg() : recipe.getUserRating());
+        });
+        ratingBar.setPartialRating(recipe.getUserRating() == 0);
+        ratingBar.setUpdateOnHover(recipe.getUserRating() == 0);
         ratingLabel.setText(String.format("Avg. Rating of %.1f/5 Over %d Ratings",
                 recipe.getRatingAvg(), recipe.getRatingCount()));
 
@@ -179,33 +191,67 @@ public class HomeController implements Initializable {
         caloriesLabel.setText(Integer.toString(recipe.calories));
 
         //Timing
-        prepTimeLabel.setText(String.format("%d min",recipe.prepTimeMinutes));
-        cookTimeLabel.setText(String.format("%d min",recipe.cookTimeMinutes));
-        totalTimeLabel.setText(String.format("%d min",recipe.totalTimeMinutes));
+        prepTimeLabel.setText(String.format("%d min", recipe.prepTimeMinutes));
+        cookTimeLabel.setText(String.format("%d min", recipe.cookTimeMinutes));
+        totalTimeLabel.setText(String.format("%d min", recipe.totalTimeMinutes));
 
         //Body
         ingredientList.getItems().addAll(recipe.ingredientsList);
-        for (int i = 0; i < recipe.instructionList.size(); i++){
+        for (int i = 0; i < recipe.instructionList.size(); i++) {
             instructionList.getItems().add(
-                    String.format("%d. %s",i + 1,recipe.instructionList.get(i)));
+                    String.format("%d. %s", i + 1, recipe.instructionList.get(i)));
         }
-        for (int i = 0; i < recipe.categories.size(); i++){
+        for (int i = 0; i < recipe.categories.size(); i++) {
             if (i == 0) {
                 categoryListLabel.setText(categoryListLabel.getText() + " " +
                         recipe.categories.get(i).getCategory());
-            }
-            else {
+            } else {
                 categoryListLabel.setText(categoryListLabel.getText() +
                         ", " +
                         recipe.categories.get(i).getCategory());
             }
         }
+        comments.setText(recipe.comments);
 
     }
     private void clearSingleRecipe(){
         ingredientList.getItems().clear();
         instructionList.getItems().clear();
         categoryListLabel.setText("Categories: ");
+    }
+    public void updateUserRating(Event event){
+        int rating = (int) Math.ceil(ratingBar.getRating());
+        ratingBar.setUpdateOnHover(false);
+
+        //Set recipe attributes
+        DbConnector.dbConnector.updateRecipeRating(rating, currentRecipe, this.userId);
+        Platform.runLater(() ->{
+            ratingBar.setRating(rating);
+
+        });
+
+        //Update recipe view
+        currentRecipe.setUserRating(rating);
+        ratingLabel.setText(String.format("Avg. Rating of %.1f/5 Over %d Ratings",
+                currentRecipe.getRatingAvg(), currentRecipe.getRatingCount()));
+
+    }
+    public void resetUserRating(Event event){
+        float rating = currentRecipe.getUserRating() == 0 ? currentRecipe.getRatingAvg() : (float) currentRecipe.getUserRating();
+        ratingBar.setRating(rating);
+    }
+    public void toggleFavorite(Event event){
+
+        if (currentRecipe.toggleFavorite()){
+            DbConnector.getDbConnector().setUserFavorite(currentRecipe, this.userId);
+        }
+        else{
+            DbConnector.getDbConnector().deleteUserFavorite(currentRecipe, this.userId);
+        }
+    }
+    public void saveComments(Event event){
+        currentRecipe.setComments(comments.getText());
+        DbConnector.getDbConnector().setComments(currentRecipe, this.userId);
     }
 
     public void openCreateModal(ActionEvent actionEvent) {
